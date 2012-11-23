d3c5e2494abb7916ea05be7d0747d780d6407299
Michał Derkacz <michal@Lnet.pl>
2012-11-23 15:17:16 +0100

    Some changes in example

--- telnet_example/main.go
+++ telnet_example/main.go
@@ -8,10 +8,26 @@ import (
 	"time"
 )
 
+const timeout = 10 * time.Second
+
 func checkErr(err error) {
 	if err != nil {
 		log.Fatalln("Error:", err)
 	}
+}
+
+func expect(t *telnet.Conn, d ...string) {
+	checkErr(t.SetReadDeadline(time.Now().Add(timeout)))
+	checkErr(t.SkipUntil(d...))
+}
+
+func sendln(t *telnet.Conn, s string) {
+	checkErr(t.SetWriteDeadline(time.Now().Add(timeout)))
+	buf := make([]byte, len(s)+1)
+	copy(buf, s)
+	buf[len(s)] = '\n'
+	_, err := t.Write(buf)
+	checkErr(err)
 }
 
 func main() {
@@ -23,26 +39,17 @@ func main() {
 
 	t, err := telnet.Dial("tcp", "127.0.0.1:23")
 	checkErr(err)
-	err = t.SetDeadline(time.Now().Add(5 * time.Second))
-	checkErr(err)
 	t.SetUnixWriteMode(true)
 
-	checkErr(t.SkipUntil("login: "))
-
-	_, err = fmt.Fprintln(t, user)
-	checkErr(err)
-
-	checkErr(t.SkipUntil("ssword: "))
-
-	_, err = fmt.Fprintln(t, passwd)
-	checkErr(err)
-
-	checkErr(t.SkipBytes('$'))
-
-	_, err = fmt.Fprintln(t, "ls -l")
-	checkErr(err)
+	expect(t, "login: ")
+	sendln(t, user)
+	expect(t, "ssword: ")
+	sendln(t, passwd)
+	expect(t, "$")
+	sendln(t, "ls -l")
 
 	ls, err := t.ReadBytes('$')
 	checkErr(err)
+
 	os.Stdout.Write(ls)
 }
